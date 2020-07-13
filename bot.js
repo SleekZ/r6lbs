881a9bba42479d5a3f1b40059f145c6ce9807300
Danny <54243564+dannyisbad@users.noreply.github.com>
2020-07-13 06:04:00 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,67 @@
 const config = require("./config.json");
 const Discord = require("discord.js");
 const fs = require("fs");
-const prefix = "r6?"
+const mysql = require("mysql");
+const prefix = "?"
 const bot = new Discord.Client();
 bot.commands = new Discord.Collection();
 
+function sleep(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+let continueit = true;
+
+let laststatus = "default";
+
+let bugged = 0;
+
+module.exports = {
+	statusChange: async function(status) {
+		if (status == "start") {
+			if (continueit) {
+				bot.user.setActivity("guus paste", { type: 'WATCHING' });
+			}
+			await sleep(5000);
+			if (continueit) {
+				bot.user.setActivity("kolsters paste", { type: 'WATCHING' });
+			}
+			await sleep(5000);
+			if (continueit) {
+				bot.user.setActivity("rose paste", { type: 'WATCHING' });
+			}
+			await sleep(5000);
+			if (continueit) {
+				bot.user.setActivity("gregj paste", { type: 'WATCHING' });
+			}
+			await sleep(5000);
+			if (continueit) {
+				bot.user.setActivity("fizzy paste", { type: 'WATCHING' });
+			}
+			await sleep(5000);
+			if (continueit) {
+				bot.user.setActivity("smartie paste", { type: 'WATCHING' });
+			}
+			await sleep(5000);
+			if (continueit) {
+				bot.user.setActivity("darky paste", { type: 'WATCHING' });
+			}
+			await sleep(5000);
+			if (continueit) {
+				bot.user.setActivity("danny paste", { type: 'WATCHING' });
+			}
+			await sleep(5000);
+			module.exports.statusChange("start");
+		} else if (status !== "continue") {
+			continueit = false;
+			console.log("STOPPING!!!");
+			bot.user.setActivity(status, { type: 'WATCHING' });
+		} else {
+			continueit = true;
+			console.log("CONTINUE!!!");
+		}
+	}
+};
 
 fs.readdir("./cmds/", (err, files) => {
 	if (err) console.error(err);
@@ -24,64 +81,109 @@ fs.readdir("./cmds/", (err, files) => {
 	});
 });
 
+var con = mysql.createConnection({
+	host: "host",
+	user: "user",
+	password: "password",
+	database: "database"
+});
+
+function checkBugged() {
+	if (bot.user.presence.game.name) {
+		let botp = bot.user.presence.game.name;
+		if (botp == laststatus) {
+			if (bugged >= 1) {
+				console.log("restarting");
+				module.exports.statusChange("start");
+				let today = new Date();
+				bot.channels.get('732163751729561632').send(`***Status changer restarted*** ${(today.getMonth()+1)}/${today.getDate()} ${today.getHours()}:${today.getMinutes()}`);
+				bugged = 0;
+				laststatus = botp;
+			} else {
+				console.log("adding 1");
+				bugged = bugged + 1;
+				laststatus = botp;
+			}
+		} else {
+			laststatus = botp;
+		}
+	}
+}
+
 bot.on("message", async message => {
 	
-	//make sure people have the proper permissions and are in the right channels to use the bot
-	if (message.guild.id !== "720615746010611752") return;
-	if (message.author.bot) return;
-	if (message.channel.type == "dm") return;
-	
-	//message logger
 	console.log("[" + message.channel.name + "] " + message.author.username + "#" + message.author.discriminator + " (" + message.author.id + "): " + message);;
 
-	//commands
+	if (message.author.bot) return;
+	if (message.channel.type == "dm") {
+		module.exports.statusChange(message.author.username + " dm me")
+		setTimeout(() => module.exports.statusChange("continue"), 5500);
+		return;
+	}
+	
+	if (message.guild.id !== "720615746010611752") return;
+	if (message.content.match(/<:.+?:\d+>/g)) return;
+	
 	let messageArray = message.content.split(" ");
 	let command = messageArray[0];			
-	let args = message.content.slice(command.length + 1);
+	let args = messageArray;
+	
 	let cmd = bot.commands.get(command.slice(prefix.length));
-	if(cmd) cmd.run(bot, message, args)	
+	if(cmd) cmd.run(bot, message, args, con)	
 	
 	if (message.member.hasPermission("ADMINISTRATOR")) return;
-		
-	//grab the message content and make it lowercase
-	let parse = message.content.toLowerCase();
 	
-	//make sure nobody can ping everyone with the bot
+	let parse = message.content.toLowerCase();
+		
 	if (parse.includes("@everyone") || parse.includes("@here")) {
 		message.delete();
 		return;
 	}
 	
-	//hahahahahahahahahaahahahahhahahahahah
-	if (parse.includes("cheat") || parse.includes("hack") || parse.includes(" esp ") || parse.includes("esp ") || parse.includes("esp") && parse.charAt(parse.length-1) == "p" && parse.charAt(parse.length-2) == "s" && parse.charAt(parse.length-3) == "e" || parse.includes("aimbot") || parse.includes("aim bot") || parse.includes("nigger") || parse.includes("nigga")/* || parse.includes("walls ") || parse == "esp"*/) {
+	if (parse.includes("cheat") || parse.includes("hack") || parse.includes("aimbot") || parse.includes("aim bot")) {
+		
 		message.delete();
 		
-	    //code telling the bot where and what to replace
+		let re = /(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-?=%.]+/;
+		
+		if (parse.match(re)) return;
+		
 		var mapObj = {
 		   "aim bot":"good aim",
 		   aimbot:"good aim",
-		   esp:"gamesense",
 		   hack:"chair",
 		   cheat:"chair",
-		   nigger:"brown person",
+		   nigger:"brown person", //this is useless
 		   nigga:"brown person"
 		};
 		
-	    //code actually replacing the words
-		let stripped = message.content.replace(/cheat|hack|aimbot|aim bot|esp|nigger|nigga/gi, function(matched){
+		let stripped = message.content.replace(/cheat|hack|aimbot|aim bot|nigger|nigga/gi, function(matched){
 		  return mapObj[matched.toLowerCase()];
 		});
 		
-	    //creating a webhook with the name and avatar of the message author, sending the stripped message and deleting it
 		message.channel.createWebhook(message.author.username, message.author.avatarURL).then(wb =>
 			wb.send(stripped).then(wb.delete())
 		);
+		
+		module.exports.statusChange(message.author.username + " try to say a blacklisted word")
+		setTimeout(() => module.exports.statusChange("continue"), 5500);
 	}
 });
 
+function startChange() {
+	let today = new Date();
+	bot.channels.get('732163751729561632').send(`*Started status changer* ${(today.getMonth()+1)}/${today.getDate()} ${today.getHours()}:${today.getMinutes()}`)
+	module.exports.statusChange("start");
+}
+
 bot.on("ready", () => {
+	let today = new Date();
 	console.log('Bot successfully loaded!')
-	bot.user.setActivity("guus pasting", { type: 'WATCHING' });
+	bot.channels.get('732163751729561632').send(`**Bot restarted** ${(today.getMonth()+1)}/${today.getDate()} ${today.getHours()}:${today.getMinutes()}`)
+	bot.user.setActivity("SEX IS STARTING TO HAPPEN!!!", { type: 'WATCHING' });
+	setTimeout(startChange, 5000);
+	checkBugged();
+	setInterval(checkBugged, 15000);
 });
 
 bot.login(config.token);
